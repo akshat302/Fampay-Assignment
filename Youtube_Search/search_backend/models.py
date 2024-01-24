dc59a99314c3b39a62ce396ea4e530a78b7dfd06
Akshat Gupta <54773056+akshat302@users.noreply.github.com>
2024-01-24 19:21:56 +0000

    created video models

--- Youtube_Search/search_backend/models.py
+++ Youtube_Search/search_backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from djongo import models as djongo_models
 
 # Create your models here.
+class VideoData(djongo_models.Model):
+    title = djongo_models.CharField(max_length=100)
+    description = djongo_models.TextField()
+    thumbnail_url = djongo_models.URLField()
+    publish_datetime = djongo_models.DateTimeField()
+    video_uuid = djongo_models.UUIDField(unique=True)
+    created_at = djongo_models.DateTimeField(auto_now_add=True)
