158ba3196586e8b425ef9d3b5a64f312693c89bc
Akshat Gupta <54773056+akshat302@users.noreply.github.com>
2024-01-26 11:45:50 +0000

    updated videodata model, replaced video_id from uuid to char field, updated max lenght of vide title, ran migrations

--- Youtube_Search/search_backend/models.py
+++ Youtube_Search/search_backend/models.py
@@ -3,9 +3,9 @@ from djongo import models as djongo_models
 
 # Create your models here.
 class VideoData(djongo_models.Model):
-    title = djongo_models.CharField(max_length=100)
+    title = djongo_models.CharField(max_length=128)
     description = djongo_models.TextField()
     thumbnail_url = djongo_models.URLField()
     publish_datetime = djongo_models.DateTimeField()
-    video_uuid = djongo_models.UUIDField(unique=True)
+    video_id = djongo_models.CharField(max_length=128, unique=True)
     created_at = djongo_models.DateTimeField(auto_now_add=True)
