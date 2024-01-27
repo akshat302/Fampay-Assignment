4304251dfcbb32a5625c6c9e87caec849ac63302
Akshat Gupta <54773056+akshat302@users.noreply.github.com>
2024-01-27 11:50:34 +0000

    addes setup code for celery to define periodic tasks

---
+++ Youtube_Search/Youtube_Search/celery.py
@@ -0,0 +1,16 @@
+import os
+from celery import Celery
+  
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Youtube_Search.settings')
+  
+celery_app = Celery('Youtube_Search')
+
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.autodiscover_tasks()
+
+celery_app.conf.beat_schedule = {
+    'fetch_video_data': {
+        'task': 'search_backend.tasks.fetch_and_store_video_data',
+        'schedule': 30.0  # It will run every 30 seconds
+    }
+}
