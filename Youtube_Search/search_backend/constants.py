c4325951154f94811c4e3298b0cb63678451a87e
Akshat Gupta <54773056+akshat302@users.noreply.github.com>
2024-01-27 11:47:53 +0000

    added constants file to store the constants, and increase the max results per api limit to 40 for youtube api

---
+++ Youtube_Search/search_backend/constants.py
@@ -0,0 +1,4 @@
+YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
+SEARCH_QUERY = "cricket"
+API_KEY_LIST = ["AIzaSyAKU8BjFW33wLEbBr98nl5ts2NLcLSujRA", "AIzaSyBBB0O-7uUePyexI1g3IVo7aKK42bSjv6Y", "AIzaSyBBB0O-7uUePyexI1g3IVo7aKK42bSjv6Y"]
+MAX_RESULTS_PER_CALL = 40
