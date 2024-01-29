44b98861484dba139379616b6f654bd15b61f379
Akshat Gupta <54773056+akshat302@users.noreply.github.com>
2024-01-29 11:12:15 +0530

    Update urls.py

--- Youtube_Search/search_backend/urls.py
+++ Youtube_Search/search_backend/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import VideoDataListView, VideoResults, SearchVideView
+from .views import VideoDataListView, SearchVideoView
 
 urlpatterns = [
     path('get_videos_list/', VideoDataListView.as_view(), name='videos_list'),
-    path('get_videos_list_by_query/', SearchVideView.as_view(), name='search_video')
+    path('get_videos_list_by_query/', SearchVideoView.as_view(), name='search_video')
 ]
 
